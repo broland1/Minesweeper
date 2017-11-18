879a260185403ee8456b1a36e776b80a04e39e4c
bluestar1996zz <bluestar1996zz@gmail.com>
2017-11-18 11:13:53 -0500

    Added bare bones documentation

--- board.pl
+++ board.pl
@@ -2,22 +2,25 @@ use strict;
 use warnings;
 
 #Class fields
-my $safe = -99;
 my $mine = -1;
 my $flag = 10;
 my $uncov = 20;
 my $invalid = -9000;
-my $map = {-1 => '*', 
+
+#Mapping of display values
+my $map = {$flag => '*', 
         0 => 'b', 1 => 'b', 2=> 'b', 3=>'b', 4=>'b', 5=>'b', 6=>'b', 7=>'b', 8=>'b',
         10 => 'f', 11 => 'f', 12=> 'f', 13=>'f', 14=>'f', 15=>'f', 16=>'f', 17=>'f', 18=>'f',
         20 => ' ', 21 => '1', 22=> '2', 23=>'2', 24=>'4', 25=>'5', 26=>'6', 27=>'7', 28=>'8',
         };
 
+#Checks to see if the space is in range (valid)
 sub valid {
   my ($self, $x, $y) = @_;
   return $self->{row} > $x and $self->{col} > $y and $x > 0 and $y > 0;
 }
 
+#Checks to see if the space is type (Depreciated)
 sub typed {
   my ($self, $x, $y) = @_;
   if ($self->valid($x,$y)){
@@ -26,23 +29,27 @@ sub typed {
   return 0;
 }
 
+#Gets what needs to be displayed on the space (Throws exception if out of bounds)
 sub getSpace {
   my ($self, $x, $y) = @_;
   return $map->{$self->get($x,$y)};
 }
 
+#Gets the raw space value (Throws exception if out of bounds)
 sub get {
   my ($self, $x, $y) = @_;
   die unless $self->valid($x,$y);
   return $self->{board}[$x][$y];
 }
 
+#Sets the raw space value (Throws exception if out of bounds)
 sub set {
   my ($self, $x, $y, $n) = @_;
   die unless $self->valid($x,$y);
   $self->{board}[$x][$y] = $n;
 }
 
+#Toggles the flag flag (Throws exception if out of bounds)
 sub toggleFlag{
   my ($self, $x, $y) = @_;
   my $val = $self->get($x,$y);
@@ -56,10 +63,12 @@ sub toggleFlag{
   }
 }
 
+#Not done
 sub generate{
   my ($self, $x, $y) = @_;
 }
 
+#Used to uncover a space and the surrounding area (Throws exception if the initial space is out of bounds)
 sub uncover{
   my ($self, $x, $y) = @_;
   my $val = $self->get($x,$y);
@@ -71,15 +80,11 @@ sub uncover{
         $self->uncover($x + $_x, $y + $_y) if $self->valid($x + $_x, $y + $_y);
       }
     }
-
   }
   return 1;
 }
 
-sub _uncover{
-  my ($self, $x, $y) = @_;
-}
-
+#Constructor
 sub new {
   my $class = shift;
   my $self = {};
